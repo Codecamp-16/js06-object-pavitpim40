4480d670be2239ce20fd40e2280a0b31fa0b2b1b
pavitpim40_mbp <pavitpim@gmail.com>
2023-12-15 13:57:48 +0700

    update:solution

---
+++ 02-Loop/04-theSentinel/main.js
@@ -0,0 +1,42 @@
+// P1-ให้เขียนโปรแกรมที่รับค่า key และ value ของ Properties ของ Object หนึ่ง
+// P2-จนกว่าจะเจอคำว่า stop ถึงหยุด [check]
+// P3-สร้าง Object
+// P4-log object นั้นออกมา [check]
+
+// P2-จนกว่าจะเจอคำว่า stop
+function terminate(key) {
+  console.log(key === 'stop');
+  return key === 'stop';
+}
+
+// P4-log object นั้นออกมา [check]
+function printObj(obj) {
+  console.log(obj);
+}
+
+// P1-ให้เขียนโปรแกรมที่รับค่า key และ value ของ Object
+// P1.5 : รับ Input ไปเรือยๆ
+// P2 : จนเจอคำว่า stop ถึงหยุด [check]
+
+function createObjectFromUser() {
+  let key;
+  let value;
+  const object = {};
+
+  do {
+    // key
+    key = prompt('Enter key');
+    if (terminate(key)) break;
+
+    // value
+    value = prompt('Enter value');
+    if (terminate(value)) break;
+    // insert key value to object
+    object[key] = value;
+  } while (true);
+  return object;
+}
+
+const obj = createObjectFromUser();
+
+console.log(obj);
